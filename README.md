c97446796c288115c07223577ba0182dea6fdac1
Jean Meira <jean.meira10@hotmail.com>
2020-07-23 15:35:11 -0300

    Site ecoleta proposto na NWL

---
+++ README.md
@@ -0,0 +1,40 @@
+# ECOLETA
+
+O ecoleta é um site criado acompanhando a primeira edição new level week (NLW), evento que acelera o aprendizado em programação que é realizado pela RocketSeat. 
+
+## RocketSeat
+RocketSeat é não só uma plataforma de educação em tecnologia, mais também uma comunidade de programadores.
+
+[RocketSeat](https://rocketseat.com.br)
+
+
+## Autores e instrutores da NLW
+
+* **RocketSeat** - [RocketSeat](https://rocketseat.com.br)
+* Diego Schell Fernandes - [Linkedin](https://www.linkedin.com/in/diego-schell-fernandes/?originalSubdomain=br)
+* Mayk Brito - [Linkedin](https://www.linkedin.com/in/maykbrito/)
+
+
+## Participação
+Ao participar da NLW desenvolvi o site proposto na trilha starter ministrada pelo Mayk Brito, e nela busquei aprimorar meus conhecimentos e aprender o que foi proposto.
+
+## Demonstração das telas
+### Tela principal
+![](./telas/tela_principal.png)
+
+### Tela de cadastro
+![](./telas/tela_cadastro.png)
+
+### Tela de busca
+![](./telas/tela_busca.png)
+
+### Tela de resultados da busca
+![](./telas/tela_resultados.png)
+
+### Tela indicando cadastro concluído
+![](./telas/cadastro_concluido.png)
+
+### Tela indicando falha no cadastro
+![](./telas/cadastro_falhou.png)
+
+Essa tela foi adicionada a mais na proposta da NLW, tendo como base a tela de cadastro concluído
---
+++ src/database/db.js
@@ -0,0 +1,82 @@
+// importar a dependência do sqlite 3
+const sqlite3 = require("sqlite3").verbose()
+
+//iniciar o objeto que irá fazer operações no banco de dados
+const  db = new sqlite3.Database("./src/database/database.db")
+
+module.exports = db
+
+
+/*
+// utilizar o objeto de banco de dados para nossas operações
+db.serialize(() => {
+
+
+  //  com comandos sql eu vou:
+  //1 criar uma tabela
+  db.run(`
+    CREATE TABLE IF NOT EXISTS places(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      image TEXT,
+      name TEXT,
+      address TEXT,
+      address2 TEXT,
+      state TEXT,
+      city TEXT,
+      items TEXT
+    );
+  `)
+
+  //2 inserir dados na tabela
+  const query = `
+    INSERT INTO places(
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+`
+
+const values = [
+  "https://images.unsplash.com/photo-1549986584-6d9e49fd6495?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  "Colectoria",
+  "Guilherme Gemballa, Jardim América",
+  "Nº 260",
+  "Santa Catarina",
+  "Rio do Sul",
+  "Resíduos Eletrônicos, Lâmpadas"
+]
+
+  function afterInsertData(err){
+    if(err){
+      return console.log(err)
+    }
+    console.log("Cadastrado com sucesso")
+    console.log(this)
+  }
+
+  //db.run(query, values, afterInsertData)
+
+  //3 consultar os dados da tabela
+  db.all(`SELECT * FROM places`, function(err,rows){
+    if(err){
+      return console.log(err)
+    }
+    console.log("Aqui estão seus registros: ")
+    console.log(rows)
+  })
+
+  //4 deletar um dado da tabela
+
+  db.run(`DELETE FROM places WHERE id = ?`,[2], function(err){
+    if(err){
+      return console.log(err)
+    }
+    console.log("Registro deletado com sucesso: ")
+  })
+  
+})
+*/
